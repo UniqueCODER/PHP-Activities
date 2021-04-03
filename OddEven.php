a6385a1ed5137d6bf4ec2f4b4a675865647c3f8b
UniqueCoder <ellyky2121@gmail.com>
2021-04-03 15:39:28 +0800

    Odd or Even limit:20000

---
+++ OddEven.php
@@ -0,0 +1,11 @@
+<?php
+      for ($x = 0; $x <=200; $x++) {
+
+            if($x%2 !=0){
+                echo  "Number ".$x." is an Odd element <br>";
+            }
+            else{
+                echo   "Number ". $x ." is an Even element <br>";
+            }
+      }      
+?>  
